09f021a7fde3227c25b0cf2b4e75b463d9e54527
alexfargo <kevin.a.fargo@gmail.com>
2024-04-29 19:55:17 -0600

    updated to use HTTPS

--- Week 8/server/index.js
+++ Week 8/server/index.js
@@ -1,10 +1,41 @@
+// const express = require("express");
+// const app = express();
+
+// const port = process.env.PORT || 3001;
+
+// app.use(express.static("public"));
+
+// app.listen(port, function () {
+//   console.log("Server started at http://localhost:%s", port);
+// });
+
+/**
+ * the original index.js is above, the the below is updated to use https
+ * details are here: https://timonweb.com/javascript/running-expressjs-server-over-https/
+ */
+
 const express = require("express");
+var fs = require("fs");
+var https = require("https");
 const app = express();
 
-const port = process.env.PORT || 3001;
+const httpPort = process.env.PORT || 4000;
+const httpsPort = process.env.HTTPS_PORT || 4443;
 
 app.use(express.static("public"));
 
-app.listen(port, function () {
-  console.log("Server started at http://localhost:%s", port);
+app.listen(httpPort, () => {
+  console.log("Server started at http://localhost:%s", httpPort);
 });
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync(__dirname + "/server.key"),
+      cert: fs.readFileSync(__dirname + "/server.cert"),
+    },
+    app
+  )
+  .listen(httpsPort, () => {
+    console.log("Server started at http://localhost:%s", httpsPort);
+  });
